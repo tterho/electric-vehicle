84554d4f5fb9b146aee394136c5f2e5de46fb0fb
Tuomas Terho <tuomas@terho.net>
2022-05-26 14:52:39 +0300

    Add input class
    - Added a class for digital inputs.

---
+++ input.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Tuomas Terho (tuomas@terho.net)
+ * All rights reserved.
+ */
+
+#include "input.h"
+
+#include <Arduino.h>
+
+/// Input pin stabilization time in milliseconds. This time prevents glitches in
+/// the input to be read as accidental changes in the input state.
+#define INPUT_STABILIZATION_DELAY_MS 50u
+
+input_c::input_c(uint8_t number, bool initial_state)
+:       m_number(number), m_previous_state(initial_state)
+{
+        pinMode(m_number, INPUT_PULLUP);
+}
+
+bool input_c::get()
+{
+        bool current_state;
+
+        current_state = digitalRead(m_number);
+
+        // Check if the current state has changed, and apply the stabilization
+        // delay when needed.
+        if (m_previous_state != current_state) {
+                delay(INPUT_STABILIZATION_DELAY_MS);
+                m_previous_state = current_state;
+        }
+
+        return m_previous_state;
+}
+
+bool input_c::has_changed()
+{
+        // Check is the digital input state changed from the previous state.
+        return (m_previous_state != digitalRead(m_number));
+}
+
+/* EOF */
---
+++ input.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Tuomas Terho (tuomas@terho.net)
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+/**
+ * @brief Input class for Arduino.
+ */
+class input_c {
+public:
+        /**
+         * @brief Constructor of the class.
+         * @param[in] number Input number.
+         * @param[in] initial_state Initial state.
+         *
+         * Initializes the input in pull-up mode.
+         */
+        explicit input_c(uint8_t number, bool initial_state);
+
+        /**
+         * @brief Get the current state.
+         * @return The state of the input.
+         *
+         * This function resets input's has-state-changed state.
+         */
+        bool get();
+
+        /**
+         * @brief Check has the state changed since last get.
+         * @retval true State has changed.
+         * @retval false State has remained unchanged.
+         */
+        bool has_changed();
+
+private:
+        /// Input number.
+        uint8_t m_number;
+        /// Previous state.
+        bool m_previous_state;
+}; // class input_c
+
+/* EOF */
