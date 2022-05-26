81b788512cbef99bf5a0466d95aa41b41fa19c7a
Tuomas Terho <tuomas@terho.net>
2022-05-26 17:42:12 +0300

    Add motor control class
    - Added a class for controlling motor throttle and direction.

---
+++ motor.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) Tuomas Terho (tuomas@terho.net)
+ * All rights reserved.
+ */
+
+#include "motor.h"
+
+motor_c::motor_c(input_c &throttle_input, input_c &direction_input,
+                 output_c &throttle_output, output_c &direction1_output,
+                 output_c &direction2_output, output_c &fw_indicator_output,
+                 output_c &bw_indicator_output)
+:       m_state(motor_state_e::OFF), m_throttle_input(throttle_input),
+        m_direction_input(direction_input), m_throttle_output(throttle_output),
+        m_direction1_output(direction1_output),
+        m_direction2_output(direction2_output),
+        m_fw_indicator_output(fw_indicator_output),
+        m_bw_indicator_output(bw_indicator_output)
+{
+}
+
+void motor_c::run()
+{
+        switch(m_state) {
+        case motor_state_e::OFF :
+                // Set direction only when the motor is off. This ensures that
+                // the direction control relays don't break.
+                set_direction_priv();
+
+                // Monitor the throttle switch. If the throttle is pressed, go
+                // to the start state.
+                if (m_throttle_input.get()) {
+                        m_state = motor_state_e::START;
+                }
+                break;
+
+        case motor_state_e::START :
+                // Set the motor power on and proceed to the running state.
+                m_throttle_output.set(true);
+                m_state = motor_state_e::RUNNING;
+                break;
+
+        case motor_state_e::RUNNING :
+                // Monitor the throttle and go to the stop state if the throttle
+                // is released.
+                if (!m_throttle_input.get()) {
+                        m_state = motor_state_e::STOP;
+                }
+                break;
+
+        case motor_state_e::STOP :
+                // Set motor power off and go to the off state.
+                m_throttle_output.set(false);
+                m_state = motor_state_e::OFF;
+                break;
+        }
+}
+
+void motor_c::set_direction_priv()
+{
+        bool direction;
+
+        direction = m_direction_input.get();
+
+        m_fw_indicator_output.set(!direction);
+        m_bw_indicator_output.set(direction);
+        m_direction1_output.set(!direction);
+        m_direction2_output.set(!direction);
+}
+
+/* EOF */
---
+++ motor.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Tuomas Terho (tuomas@terho.net)
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include "input.h"
+#include "output.h"
+
+/**
+ * @brief Motor direction and throttle control class.
+ */
+class motor_c {
+public:
+        /**
+         * @brief Motor running states.
+         */
+        enum class motor_state_e {
+                /// Motor is off.
+                OFF = 0,
+                /// Motor is starting.
+                START,
+                /// Motor is running.
+                RUNNING,
+                /// Motor is stopping.
+                STOP
+        }; // enum class motor_state_e
+
+        /**
+         * @brief Constructor of the class.
+         * @param[in] throttle_input A reference to a throttle switch input.
+         * @param[in] direction_input A reference to a direction switch input.
+         * @param[in] throttle_output A reference to a throttle output.
+         * @param[in] direction1_output A reference to a direction control 1
+         *      output.
+         * @param[in] direction2_output A reference to a direction control 2
+         *      output.
+         * @param[in] fw_indicator_output A reference to a forward indicator
+         *      output.
+         * @param[in] bw_indicator_output A reference to a backward indicator
+         *      output.
+         */
+        motor_c(input_c &throttle_input, input_c &direction_input,
+                output_c &throttle_output, output_c &direction1_output,
+                output_c &direction2_output, output_c &fw_indicator_output,
+                output_c &bw_indicator_output);
+
+        /**
+         * @brief Run motor control.
+         */
+        void run();
+
+private:
+        /**
+         * @brief Update and set the direction control.
+         */
+        void set_direction_priv();
+
+        /// Motor state.
+        motor_state_e m_state;
+        /// Throttle switch input.
+        input_c &m_throttle_input;
+        /// Direction switch input.
+        input_c &m_direction_input;
+        /// Throttle output.
+        output_c &m_throttle_output;
+        /// Direction 1 output.
+        output_c &m_direction1_output;
+        /// Direction 2 output.
+        output_c &m_direction2_output;
+        /// Forward indicator output.
+        output_c &m_fw_indicator_output;
+        /// Backward indicator output.
+        output_c &m_bw_indicator_output;
+}; // class power_c
+
+/* EOF */
